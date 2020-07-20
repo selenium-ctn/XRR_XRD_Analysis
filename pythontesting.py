35123b9a5e18030b7f0157c0afedfa970cb589cb
selincetin <44755643+selincetin@users.noreply.github.com>
2020-07-20 18:48:45 -0400

    smooth derivs

--- pythontesting.py
+++ pythontesting.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import scipy.signal
 from scipy.interpolate import interp1d
 from scipy.ndimage import median_filter
-from csaps import csaps
+from csaps import CubicSmoothingSpline
+from scipy.interpolate import UnivariateSpline
 
 #zscan = open('Smartlab data/ZscanSTBXRR_0014_Scan2020Jan24-124745.dat', 'r')
 zscan = open('Smartlab data/Zscan_0007_Scan2020Jan23-191605.dat', 'r')
@@ -58,43 +59,57 @@ print(zscan_cps)
 #print(bkg_theta)
 #print(bkg_cps)
 
-#my_pwlf = pwlf.PiecewiseLinFit(zscan_z, zscan_cps)
-#breaks = my_pwlf.fit(3)
-#print(breaks)
-
 first_deriv = np.gradient(zscan_cps, zscan_z)
 
-test = scipy.signal.savgol_filter(zscan_cps, 101, polyorder=7, deriv=0)
-testgrad = np.gradient(test, zscan_z) 
+#test = scipy.signal.savgol_filter(zscan_cps, 101, polyorder=7, deriv=0)
+#testgrad = np.gradient(test, zscan_z) 
 
-tst = scipy.ndimage.median_filter(zscan_cps, size=5)
-tstg = np.gradient(tst, zscan_z)
+#tst = scipy.ndimage.median_filter(zscan_cps, size=5)
+#tstg = np.gradient(tst, zscan_z)
 
-tstgg = scipy.ndimage.median_filter(tstg, size=5)
-tstg2 = scipy.signal.savgol_filter(tstg, 101, polyorder=7, deriv=0)
+#tstgg = scipy.ndimage.median_filter(tstg, size=5)
+#tstg2 = scipy.signal.savgol_filter(tstg, 101, polyorder=7, deriv=0)
 
-tstggg = scipy.ndimage.median_filter(first_deriv, size=5)
+#tstggg = scipy.ndimage.median_filter(first_deriv, size=5)
 z2 = np.linspace(zscan_z[0], zscan_z[zscan_z.size - 1], zscan_z.size)
 
-cp = csaps(zscan_z, zscan_cps, z2, smooth=0.9997)
+cp = CubicSmoothingSpline(zscan_z, zscan_cps, smooth=0.99995).spline
+cpgrad = cp.derivative(nu=1)
+
+cp2 = CubicSmoothingSpline(zscan_z, first_deriv, smooth=0.9995).spline
+
+spl = UnivariateSpline(zscan_z, zscan_cps, k=4, s= 1e15)
+
+cpnewt = np.gradient(spl(z2), z2)
+#dd22 = np.gradient(cpnewt, z2)
+dd3 = np.gradient(cpgrad(z2), z2)
+dd4 = np.gradient(cp2(z2), z2)
+
 
 #plt.plot(zscan_z, zscan_cps)
 #plt.plot(zscan_z, tst)
 #plt.plot(zscan_z, test)
-plt.plot(zscan_z, zscan_cps, 'o', z2, cp, '-')
+plt.plot(zscan_z, zscan_cps, 'o', z2, cp(z2), '-')
+plt.plot(z2, spl(z2))
 plt.figure()
 #plt.plot(zscan_z, ftwo(x2))
 #plt.figure()
-plt.plot(zscan_z, first_deriv)
+#plt.plot(zscan_z, first_deriv)
+plt.plot(z2, cpgrad(z2))
+plt.plot(z2, cp2(z2))
+plt.plot(z2, cpnewt)
 #plt.plot(zscan_z, tstg)
-plt.plot(zscan_z, tstg2)
-plt.plot(zscan_z, testgrad)
+#plt.plot(zscan_z, tstg2)
+#plt.plot(zscan_z, testgrad)
 #plt.plot(zscan_z, newt)
 #plt.plot(zscan_z, tstgg)
-#plt.plot(zscan_z, tstggg)
+
 plt.figure()
-plt.plot(zscan_z, np.gradient(tstg2, zscan_z))
-plt.plot(zscan_z, np.gradient(testgrad, zscan_z))
+#plt.plot(z2, dd22)
+#plt.plot(z2, dd3)
+plt.plot(z2, dd4) # yes good 
+#plt.plot(zscan_z, np.gradient(tstg2, zscan_z))
+#plt.plot(zscan_z, np.gradient(testgrad, zscan_z))
 #plt.figure()
 plt.show()
 
