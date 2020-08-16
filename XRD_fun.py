e56899f053b611f33cb0adbde4746e7c286c0e6c
selincetin <44755643+selincetin@users.noreply.github.com>
2020-08-16 17:56:45 -0400

    new bragg peak finding method

--- XRD_fun.py
+++ XRD_fun.py
@@ -1,4 +1,4 @@
-from scipy.signal import argrelextrema, find_peaks, peak_prominences, find_peaks_cwt
+from scipy.signal import argrelextrema, find_peaks, peak_prominences, find_peaks_cwt, peak_widths
 import heapq 
 import numpy as np
 from math import pi
@@ -119,22 +119,100 @@ def lorentz(x, *p):
     return y0 + (2 * A / pi) * (w / (4 * np.power((x - xc), 2) + np.power(w, 2)))
 
 def find_bragg_peak_alt(q, cps):
-    #peaks, _ = find_peaks(cps)
-    #prominences = peak_prominences(cps, peaks)[0]
-    #contour_heights = cps[peaks] - prominences
-    #plt.plot(q, cps)
-    #plt.plot(peaks, cps[peaks], "x")
-    #plt.vlines(x=peaks, ymin=contour_heights, ymax=cps[peaks])
-
     #not sure how big I should make the widths....ask Carlos about max bragg peak sizes
-    peaks = find_peaks_cwt(cps, np.linspace(1, 20))
-    prom, lb, rb = peak_prominences(cps, peaks, wlen=20)
+    #remove NaN?
+    #find the peaks (maxima) in the original data (reflectivity). Use cwt because of the possible noise. orig_peaks = array of positions,
+    #referring to positions in the cps array where the peaks are located 
+    orig_peaks = find_peaks_cwt(cps, np.linspace(1, 20, num=50))
+
+    #find the peaks (maxima) in the first derivative of the reflectivity data. Use cwt because of the possible noise. peaks_1d = array of 
+    #positions, referring to positions in the first_deriv array where the peaks are located 
+    first_deriv = np.gradient(cps, q)
+    peaks_1d =  find_peaks_cwt(first_deriv, np.linspace(1, 10, num=50))
+
+    #find the widths (and other associtated values) of the peaks in the first dervative. This is more accurate than finding the widths of the peaks in the original 
+    #data. rel_height=1 means the left and right bases are being found at the lowest contour lines of the peak  
+    widths, width_heights, left_ips, right_ips = peak_widths(first_deriv, peaks_1d, rel_height=1)
+
+    #create an array of the values of the peaks 
+    loc_max_vals = first_deriv[peaks_1d]
+
+    #find the two highest peak values 
+    max_2_vals = heapq.nlargest(2, loc_max_vals)
+
+    #find the position of the array containing the values of the peaks associated with the highest peak (also corresponds to position
+    #in array of positions of peak values in first_deriv)
+    loc_max_pos_1 = np.where( loc_max_vals == max_2_vals[0])
+
+    #find position of highest peak in first_deriv array 
+    peaks_1d_pos_1 = peaks_1d[loc_max_pos_1]
+
+    #repeat process for second highest peaks 
+    loc_max_pos_2 = np.where(loc_max_vals == max_2_vals[1])
+    peaks_1d_pos_2 = peaks_1d[loc_max_pos_2]
+
+    #same process as above, but with the original data instead of the first derivative
+    loc_max_v = cps[orig_peaks]
+    max2vals = heapq.nlargest(2, loc_max_v)
+    loc_max_p1 = np.where(loc_max_v == max2vals[0])
+    orig_peaks_pos_1 = orig_peaks[loc_max_p1]
+    loc_max_p2 = np.where(loc_max_v == max2vals[1])
+    orig_peaks_pos_2 = orig_peaks[loc_max_p2]
+
+    #find the positions of the peaks directly preceeding the highest and second highest peaks in the original data
+    prev_peak_1_pos = orig_peaks[loc_max_p1[0] - 1]
+    prev_peak_2_pos = orig_peaks[loc_max_p2[0] - 1]
+
+    #confirm that both of the highest peaks found in the first derivative correspond to the highest peaks in the original data.
+    #do this by seeing if the q values of the first derivative peaks fall between the q value of the peak and its preceeding peak
+    #peaks in the first derivative correspond will always correspond to a point on the original data peak before (to the right of) the 
+    #highest point of the peak 
+    if q[peaks_1d_pos_1] < q[orig_peaks_pos_1] and q[peaks_1d_pos_1] > q[prev_peak_1_pos]:
+        bragg_peak_possibility_1 = peaks_1d_pos_1
+    elif q[peaks_1d_pos_1] < q[orig_peaks_pos_2] and q[peaks_1d_pos_1] > q[prev_peak_2_pos]:
+        bragg_peak_possibility_1 = peaks_1d_pos_1
+    else: bragg_peak_possibility_1 = None
+
+    if q[peaks_1d_pos_2] < q[orig_peaks_pos_2] and q[peaks_1d_pos_2] > q[prev_peak_2_pos]:
+        bragg_peak_possibility_2 = peaks_1d_pos_2
+    elif q[peaks_1d_pos_2] < q[orig_peaks_pos_1] and q[peaks_1d_pos_2] > q[prev_peak_1_pos]:
+            bragg_peak_possibility_2 = peaks_1d_pos_2
+    else: bragg_peak_possibility_2 = None
+
+    #if both peaks correspond correctly (the substrate peak may or may not register as a highest 1st derivative peak), compare the widths
+    #of the peaks in the first derivative. The bragg peak will be the one that is wider
+    if bragg_peak_possibility_1 != None and bragg_peak_possibility_2 != None:
+        #needs testing...didn't get tested bc substrate peak did not register 
+        if widths[loc_max_pos_1] > widths[loc_max_pos_2]:
+            bragg_peak_1d = loc_max_pos_1
+        else:
+            bragg_peak_1d = loc_max_pos_2
+    elif bragg_peak_possibility_1 != None:
+        bragg_peak_1d = loc_max_pos_1
+    elif bragg_peak_possibility_2 != None:
+        bragg_peak_1d = loc_max_pos_2
+
+    #adjust for the fact that the widths will sometimes surpass the 0 mark in the first derivative. 
+    if first_deriv[left_ips[bragg_peak_1d].astype(np.int)] < 0:
+        curr_index = left_ips[bragg_peak_1d].astype(np.int)
+        adjust = 0 
+        while first_deriv[curr_index] < 0:
+            curr_index = curr_index + 1
+            adjust = adjust + 1
+    
+    print((q[curr_index], q[(right_ips[bragg_peak_1d] - adjust + widths[bragg_peak_1d]).astype(np.int)]))
+    
     plt.figure()
     plt.plot(q, cps)
-    plt.plot(q[peaks], cps[peaks], "x")
-    plt.plot(q[lb], cps[lb], 'o')
-    plt.plot(q[rb], cps[rb], 'o')
+    plt.plot(q[orig_peaks], cps[orig_peaks], "x")
     plt.yscale("log")
+    #plt.plot(q[lb], cps[lb], 'o')
+    #plt.plot(q[rb], cps[rb], 'o')
+    plt.figure()
+    plt.plot(q, first_deriv)
+    plt.plot(q[peaks_1d], first_deriv[peaks_1d], "x")
+    plt.hlines(width_heights, q[left_ips.astype(np.int)], q[right_ips.astype(np.int)])
+
 
 
     
