2ba2697877b5b8d14deeca51084169ba541cede5
selincetin <44755643+selincetin@users.noreply.github.com>
2020-08-29 16:30:35 -0400

    uh oh why won't files open

--- XRR_GUI.py
+++ XRR_GUI.py
@@ -8,6 +8,9 @@ import XRR_Analysis_Compat as XAC
 
 class GUI:
     def __init__(self, master):
+        global tk_zscan
+        tk_zscan = tk.StringVar()
+        tk_zscan.set("No file chosen")
         self.master = master 
         master.title("XRR/XRD Data Reduction")
         tabControl = ttk.Notebook(root)
@@ -15,21 +18,28 @@ class GUI:
         xrd_tab = ttk.Frame(tabControl)
         tabControl.add(xrr_tab, text='XRR')
         tabControl.add(xrd_tab, text='XRD')
-        tabControl.pack(expand=1, fill="both")
-        self.label = ttk.Label(xrr_tab, text="test baybeee")
-        self.label.pack()
-        self.button = ttk.Button(text = "Select zscan file",command = self.fileDialogZscan)
-        self.button.pack()
-        self.button = ttk.Button(text = "Select specular file",command = self.fileDialogSpec)
-        self.button.pack()
-        self.button = ttk.Button(text = "Select background file",command = self.fileDialogBkg)
-        self.button.pack()
+        #tabControl.pack(expand=1, fill="both")
+        tabControl.grid(sticky="W")
+        self.button = ttk.Button(xrr_tab, text = "Select zscan file",command = self.fileDialogZscan)
+        self.button.grid(pady=2.5, sticky="W", row=1, column=0)
+        self.label = ttk.Label(xrr_tab, textvariable=tk_zscan)
+        #self.label.grid(row=1, column=1)
+        #self.button.pack()
+        #self.label = ttk.Label(xrr_tab, text=zscan)
+        #self.label.pack()
+        self.button = ttk.Button(xrr_tab, text = "Select specular file",command = self.fileDialogSpec)
+        self.button.grid(pady=2.5, sticky="W")
+        self.button = ttk.Button(xrr_tab, text = "Select background file",command = self.fileDialogBkg)
+        self.button.grid(pady=2.5, sticky="W")
         self.button = ttk.Button(text = "Run",command = self.run)
-        self.button.pack()
+        #self.button.pack()
 
     def fileDialogZscan(self):
         global zscan
+        #zscan = "empty"
         zscan = filedialog.askopenfile(initialdir = "/", title="Select zscan file", filetypes = (("dat files","*.dat"),("text files","*.txt"), ("all files","*.*")), mode="r")
+        #if zscan != "empty":
+         #   tk_zscan.set(zscan)
         #printtext(zscan)
         #test2.zscan = zscan
 
@@ -55,50 +65,3 @@ class GUI:
 root = tk.Tk()
 gui = GUI(root)
 root.mainloop()
-
-class xrr_tab:
-    def __init__(self, master):
-        self.master = master 
-        self.frame = tk.Frame(self.master)
-        #self.minsize(640, 400)
-        
-        self.labelFrame = ttk.LabelFrame(self, text = "Open File")
-        self.labelFrame.grid(column = 0, row = 1, padx = 20, pady = 20)
-        self.button = ttk.Button(self.labelFrame, text = "Browse A File",command = self.fileDialog)
-        self.button.grid(column = 1, row = 1)
- 
- 
-    def fileDialog(self):
- 
-        self.zscan = filedialog.askopenfile(initialdir = "/", title="Select zscan file", filetypes = (("dat files","*.dat"),("text files","*.txt"), ("all files","*.*")))
-        self.label = ttk.Label(self.labelFrame, text = "")
-        self.label.grid(column = 1, row = 2)
-        self.label.configure(text = self.zscan)
- 
-def main():
-    root = tk.Tk()
-    root.title("XRR/XRD Data Reduction")
-    tabControl = ttk.Notebook(root)
-    xrr_tab = ttk.Frame(tabControl)
-    xrd_tab = ttk.Frame(tabControl)
-    tabControl.add(xrr_tab, text='XRR')
-    tabControl.add(xrd_tab, text='XRD')
-    tabControl.pack(expand=1, fill="both")
-    root.mainloop()
-
-#main()
-
-#parent window
-#root = tk.Tk()
-#root.title("XRR/XRD Data Reduction")
-#tabControl = ttk.Notebook(root)
-#xrr_tab = ttk.Frame(tabControl)
-#xrd_tab = ttk.Frame(tabControl)
-#tabControl.add(xrr_tab, text='XRR')
-#tabControl.add(xrd_tab, text='XRD')
-#tabControl.pack(expand=1, fill="both")
-#use a button or drop down so people can change if they choose the wrong one
-#xrr_tab.zscan = filedialog.askopenfile(initialdir = "/", title="Select zscan file", filetypes = (("dat files","*.dat"),("text files","*.txt"), ("all files","*.*")))
-#xrr_tab.specular = filedialog.askopenfile(initialdir = "/", title="Select specular file", filetypes = (("dat files","*.dat"),("text files","*.txt"), ("all files","*.*")))
-#xrr_tab.bkg = filedialog.askopenfile(initialdir = "/", title="Select background file", filetypes = (("dat files","*.dat"),("text files","*.txt"), ("all files","*.*")))
-#root.mainloop()
